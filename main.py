145ea05c6da217205f27e78bf8ef78d178695f33
Nicholas Koh <nicholaskohwx@gmail.com>
2019-09-22 20:12:31 +0800

    GUI update and read csv
    Added ability to read file path from user input by text/explorer.
    
    Read CSV file using file path supplied.

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
-import Tkinter
-from Tkinter import *
 import tkFileDialog
-
-window = Tk()
-window.title("Assignment")
-window.geometry('500x500')
-lbl = Label(window, text="Label1")
-lbl.grid(column=0, row=0)
-in_path = None
+from Tkinter import *
 
 
-def clicked():
-    in_path = tkFileDialog.askopenfilename()
-    lbl.configure(text=in_path)
+class Application(Frame):
+    def fileSelect(self):
+        self.text.delete(0, END)
+        self.text.insert(0, tkFileDialog.askopenfilename())
+        return
+
+    def getText(self):
+        global path
+        path = self.text.get()
+        self.quit()
+
+    def createWidgets(self):
+        # Create quit button
+        self.NEXT["text"] = "Next"
+        self.NEXT["fg"] = "#1d7a3f"
+        self.NEXT["command"] = self.getText
+        self.NEXT.pack({"side": "right"})
+
+        # Create file button
+        self.hi_there["text"] = "Select CSV File"
+        self.hi_there["command"] = self.fileSelect
+        self.hi_there.pack({"side": "right"})
+
+        # Create text field
+        self.text["width"] = 50
+        self.text.pack({"side": "right"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+
+        # initialising widgets
+        self.hi_there = Button(self)
+        self.NEXT = Button(self)
+        self.text = Entry(self)
+
+        self.pack(expand=1)
+        self.createWidgets()
 
 
-btn = Button(window, text="Click Me", command=clicked)
-btn.grid(column=1, row=0)
-window.mainloop()
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+print "Output path after app exit: " + path
+csv1 = None
+try:
+    csv1 = open(path)
+except IOError:
+    print "Please enter valid file path!"
+else:
+    headers = csv1.readline().split(',')
+    for line in csv1.readlines():
+        print line
+    csv1.close()
