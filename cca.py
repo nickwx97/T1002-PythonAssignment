1af038e378f0af5adac34a48930c15eb2355f9c6
Nicholas Koh <nicholaskohwx@gmail.com>
2019-10-01 22:20:37 +0800

    Added search function for CCA
    Signed-off-by: Nicholas Koh <nicholaskohwx@gmail.com>

--- cca.py
+++ cca.py
@@ -3,6 +3,19 @@ from readCSV import *
 
 class CCA:
 
+    def searchDict(self, key="cca_generic_name", value=""):
+        if key not in self.__headers:
+            print "Invalid search critera"
+            return
+        result = []
+        for x in self.__dict:
+            for y in self.__dict.get(x):
+                for z in y:
+                    if z == key:
+                        if value.upper() in y.get(z):
+                            result.append(x)
+        return result
+
     def getDict(self): return self.__dict
 
     # split into dict
@@ -19,14 +32,17 @@ class CCA:
                     temp[headers[c]] = r[c]
                 self.__dict[r[0]].append(temp)
 
-        del headers, values, self.__raw  # Free up memory
+        self.__headers = headers
+        del values, self.__raw  # Free up memory
 
     def __init__(self):
         # self.raw = openCSV(csvPath())
         self.__raw = openCSV("Data/co-curricular-activities-ccas.csv")  # laziness is real, for testing purposes... :)
+        self.__headers = None
         self.__dict = None
 
 
 cca = CCA()
 cca.genDict()
-print cca.getDict()
+# print cca.getDict()
+print cca.searchDict(value="Basketball")
