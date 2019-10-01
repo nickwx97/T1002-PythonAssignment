c5565ab638ba3739fc74a0848f4a22577b98f57a
derekyjj <55313529+derekyjj@users.noreply.github.com>
2019-10-01 12:57:22 +0800

    Update GUI.py
    basic search and print to gui

--- GUI.py
+++ GUI.py
@@ -2,65 +2,41 @@ import Tkinter
 import csv
 
 
-class App(Tkinter.Frame):
-    def __init__(self, master=None):
-        Tkinter.Frame.__init__(self, master)
-        self.master = master
-        self.pack(anchor="w")
+def search(event=None):
+    schEnquiry = school.get()
 
-        Tkinter.Label(self, text="School: ").grid(row=2, column=1, sticky="w")
+    schoolDict = {}
 
-        self.entry = Tkinter.StringVar()
-        schoolentry = Tkinter.Entry(self, textvariable=self.entry)
-        schoolentry.grid(row=3, column=4, sticky="w")
+    with open('Data/general-information-of-schools.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # schoolDict called to store information of school from csv file with the school name as the key
+            schoolDict[row['school_name']] = row
 
+    # schInfoArr is the list of all the information of the requested school with the school name as the key
+    schInfoArr = list(v for k, v in schoolDict.iteritems() if schEnquiry in k.lower())
+    # START test result print
 
+    for row in schInfoArr:
+        x = "%50s%40s" % (row['school_name'], row['address']) + "\n"
+        print x
+        Tkinter.Label(app, text=x).pack()
 
-
-root = Tkinter.Tk()
-app = App(root)
-root.minsize(500, 200)
-root.title = "GUI"
-root.mainloop()
-"""        
 app = Tkinter.Tk()
-# to rename the title of the window
-app.title("GUI")
+app.minsize(800, 200)
+app.title = "GUI"
+
+s1 = Tkinter.Label(app, text="School Name")
+s1.grid(row=2, column=0)
+
+school = Tkinter.StringVar()
+inputs = Tkinter.Entry(app, textvariable=school)
+inputs.grid(row=2, column=1)
 
 
-def function():
-    areaDict = {}
-    with open('C:\Users\Derek\Downloads\school-directory-and-information\general-information-of-schools.csv',
-              'r') as csvfile:
-        data = csv.DictReader(csvfile)
-        for row in data:
-            if row['dgp_code'] == 'BEDOK':
-                Bedok = row['school_name']
-        print Bedok
-    csvfile.close()
+b1 = Tkinter.Button(app, text="Search", width=10, command=search)
+b1.grid(row=2, column=2)
 
 
-app_menu = Tkinter.Menu(app)
-app.config(menu=app_menu)
-app.geometry("600x300")
-app.resizable(0, 0)
+app.mainloop()
 
-find_menu = Tkinter.Menu(app_menu)
-area_menu = Tkinter.Menu(app_menu)
-app_menu.add_cascade(label="Find", menu=find_menu)
-find_menu.add_command(label="School", command=app.quit())
-
-app_menu.add_cascade(label="Area", menu=area_menu)
-area_menu.add_command(label="Bedok", command=function())
-
-
-
-
-edit_menu = Tkinter.Menu()
-# pack is used to show the object in the window
-top_frame = Tkinter.Frame(app).pack()
-bottom_frame = Tkinter.Frame(app).pack()
-
-label = Tkinter.Label(app, text="Hello World!12312313131312312313").pack()
-
-"""
