ea1f2d9bfb2a211349d1de82a16f637f9a7bddd2
derekyjj <55313529+derekyjj@users.noreply.github.com>
2019-10-01 14:34:14 +0800

    Update GUI.py
    added error checking for empty input

--- GUI.py
+++ GUI.py
@@ -1,42 +1,42 @@
 import Tkinter
+import tkMessageBox
 import csv
-
 
 def search(event=None):
     schEnquiry = school.get()
-
     schoolDict = {}
+    if schEnquiry == "":
+        tkMessageBox.showerror("Error", "Please enter a valid input")
+    else:
+        with open('Data/general-information-of-schools.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                # schoolDict called to store information of school from csv file with the school name as the key
+                schoolDict[row['school_name']] = row
 
-    with open('Data/general-information-of-schools.csv', 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            # schoolDict called to store information of school from csv file with the school name as the key
-            schoolDict[row['school_name']] = row
+        # schInfoArr is the list of all the information of the requested school with the school name as the key
+        schInfoArr = list(v for k, v in schoolDict.iteritems() if schEnquiry in k.lower())
+        # START test result print
+        print schInfoArr[0]
+        for row in schInfoArr:
+            x = "%50s%40s" % (row['school_name'], row['address']) + "\n"
+            print x
+            Tkinter.Label(app, text=x).pack()
 
-    # schInfoArr is the list of all the information of the requested school with the school name as the key
-    schInfoArr = list(v for k, v in schoolDict.iteritems() if schEnquiry in k.lower())
-    # START test result print
-
-    for row in schInfoArr:
-        x = "%50s%40s" % (row['school_name'], row['address']) + "\n"
-        print x
-        Tkinter.Label(app, text=x).pack()
 
 app = Tkinter.Tk()
-app.minsize(800, 200)
+app.minsize(1200, 400)
 app.title = "GUI"
 
 s1 = Tkinter.Label(app, text="School Name")
-s1.grid(row=2, column=0)
+s1.grid(row=1, column=1)
 
 school = Tkinter.StringVar()
 inputs = Tkinter.Entry(app, textvariable=school)
-inputs.grid(row=2, column=1)
+inputs.grid(row=1, column=2)
 
-
-b1 = Tkinter.Button(app, text="Search", width=10, command=search)
-b1.grid(row=2, column=2)
-
+b1 = Tkinter.Button(app, text="Search", width=7, command=search)
+b1.grid(row=1, column=3)
 
 app.mainloop()
 
