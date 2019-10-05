10e3755b7942db28474aba33fa1ff8e14fbf9eb9
puglord96 <thegamemaster123@gmail.com>
2019-10-05 17:08:36 +0800

    Update schInfoGUIFunction.py

--- schInfoGUIFunction.py
+++ schInfoGUIFunction.py
@@ -11,28 +11,27 @@ def callTk():
 
     return top
 
-def messageBox(x):
-    top = callTk()
-    frame = Frame(top)
+def messageBox(x,y):
+    frame = Frame(y)
     frame.place(rely=0.1, relheight=0.6, relwidth=1.0)
     var = StringVar()
     label = Message(frame, textvariable=var, bd=6, relief=SUNKEN)
 
-    top.geometry("1000x500")
-    top.title("School Guide")
+    y.geometry("1000x500")
+    y.title("School Guide")
 
     var.set(x)
     label.pack(side=TOP)
     label.place(y=80, relwidth=1, height=200)
-    top.mainloop()
+    y.mainloop()
 
-def printInfo(x):
+def printInfo(x,y):
     try:
         if x == "":
             messageBox("")
         else:
             schstuff = schoolInfo.schoolstuff(x)
-            messageBox(schstuff)
+            messageBox(schstuff,y)
 
     except:
         messageBox("No such school found!")
