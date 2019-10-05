4dffcc4b57d2701062609b9e1ad8f5c1f81e052b
puglord96 <thegamemaster123@gmail.com>
2019-10-05 17:25:52 +0800

    Update schoolByConditionGUI.py

--- schoolByConditionGUI.py
+++ schoolByConditionGUI.py
@@ -2,6 +2,8 @@ from Tkinter import *
 from schoolByCondition import *
 from schInfoGUIFunction import *
 
+"""appendArr uses the function schoolstuff from schoolByCondition to populate the array for populating the listbox.
+If you want to change the filter condition, please change it in schoolByCondition.py"""
 def appendArr(x,y):
     del y[:]
     schNameArr = schoolstuff(x)
@@ -11,17 +13,17 @@ def appendArr(x,y):
 
     return y
 
-def schListBox(y,z):
+def schListBox(x,y):
+    """schListBox populates the listbox based on the search substring. x is the search substring and y is the name of
+    the array to be processed"""
     Lb1.delete(0, END)
     if not y:
         Lb1.delete(0, END)
     else:
-        schNameArr = appendArr(y, z)
+        schNameArr = appendArr(x, y)
         for schName in schNameArr:
             Lb1.insert(END, schName[0])
 
-def printStr(x):
-    print x
 
 schArray = []
 
@@ -38,10 +40,13 @@ frame = Frame(top)
 frame.place(rely=0.5, relheight=0.6, relwidth=1.0)
 
 Lb1 = Listbox(top,height=20,width=50)
-Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(schArray[Lb1.curselection()[0]][0],Toplevel()))
+
+"""Lb1.bind contains the onclick event of the listBox. Lb1.curselection() is just the position number of each member 
+in the listbox i.e first member will be position number 0 and so on"""
+Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(schArray[Lb1.curselection()[0]][0],Toplevel()))  # Toplevel() just lets the function to be opened in a new window
 Lb1.pack()
 
-
+"""Creates an empty listbox when the program is opened"""
 schListBox("","")
 
 
