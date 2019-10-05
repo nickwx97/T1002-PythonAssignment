86a8b483601c65b9a7ac0d9310ebf5bdf8585bc0
elishazacarias <55310442+elishazacarias@users.noreply.github.com>
2019-10-05 21:55:52 +0800

    added function to search subjects in level (e.g. primary, secondary)

--- subjects-offered.py
+++ subjects-offered.py
@@ -1,5 +1,5 @@
 from csv import *
-
+from schoolByCondition import *
 
 class SubjectsOffered:
     def getDict(self):
@@ -11,7 +11,7 @@ class SubjectsOffered:
             if i.upper() == school_name.upper():
                 for x in self.__dict[i]:
                     subjects.append(x[self.__headers[1]])
-        return subjects
+        return subjects # returns an array
 
     def getSchoolsBySubjectDesc(self, subject_name):
         _subj_name = subject_name.upper()
@@ -22,11 +22,27 @@ class SubjectsOffered:
                     school_arr.append(i)
         return school_arr
 
+    def getSubjectsByLevel(self, level):
+        # gets all schools under specified level
+        x = schoolstuff(level, 'mainlevel_code')
+        schools_arr = []
+        for y in range(len(x)):
+            schools_arr.append(x[y][0])
+        # stores all subjects (non-duplicated)
+        subjects_arr = []
+        for item in schools_arr:
+            subjects_temp = self.getSubjectsBySchoolName(item)
+            for i in subjects_temp:
+                if i not in subjects_arr:
+                    subjects_arr.append(i)
+        return subjects_arr
+
     # split into dict
     def createDict(self):
         x = genDict(self.__raw)
         self.__dict = x[1]
         self.__headers = x[0]
+
 
     def __init__(self):
         # self.rw = openCSV(csvPath())
@@ -37,4 +53,5 @@ class SubjectsOffered:
 
 so = SubjectsOffered()
 so.createDict()
-# print so.getDict()
+print so.getSubjectsByLevel('secondary')
+# so.getSubjectsBySchoolName('assumption english school')
