0cc7d4a1dbcc25aa3df9fa78504a498f40f2e71a
puglord96 <thegamemaster123@gmail.com>
2019-10-06 00:47:54 +0800

    Update schoolByConditionGUI.py

--- schoolByConditionGUI.py
+++ schoolByConditionGUI.py
@@ -5,31 +5,37 @@ from schoolByCondition import *
 If you want to change the filter condition, please change it in schoolByCondition.py"""
 
 
-def appendArr(x, y):
+def appendArr(x, y, z):
     del y[:]
-    sch_name_arr = schoolstuff(x)
+    sch_name_arr = schoolstuff(x,z)
 
     for name in sch_name_arr:
+        # print name
         y.append(name)
 
     return y
 
 
-def schListBox(x, y):
+
+def schListBox(x,y,z):
     """schListBox populates the listbox based on the search substring. x is the search substring and y is the name of
     the array to be processed"""
+
+
     Lb1.delete(0, END)
-    if not y:
-        Lb1.delete(0, END)
-    else:
-        sch_name_arr = appendArr(x, y)
-        for schName in sch_name_arr:
-            Lb1.insert(END, schName[0])
+    printArr = []
+    appendArr(x, printArr, z)
+    for schName in printArr:
+        Lb1.insert(END, schName[0])
+        y.append(schName)
 
+    # for z in y:
+    #     print z
 
-schArray = []
 
 top = Tk()
+
+
 
 top.geometry("1000x500")
 
@@ -38,21 +44,25 @@ L1.pack()
 E1 = Entry(top, bd=5)
 E1.pack()
 
+
+schArray = []
+
+
 frame = Frame(top)
 frame.place(rely=0.5, relheight=0.6, relwidth=1.0)
 
 Lb1 = Listbox(top, height=20, width=50)
-
+# appendArr('bedok',schArray,'dgp_code')
 """Lb1.bind contains the onclick event of the listBox. Lb1.curselection() is just the position number of each member 
 in the listbox i.e first member will be position number 0 and so on"""
-Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(schArray[Lb1.curselection()[0]][0],
-                                                      Toplevel()))  # Toplevel() just lets the function to be opened in a new window
+Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(schArray[Lb1.curselection()[0]][0],Toplevel()))  # Toplevel() just lets the function to be opened in a new window
 Lb1.pack()
 
 """Creates an empty listbox when the program is opened"""
-schListBox("", "")
+# schListBox("","dgp_code")
 
-B = Button(top, text="Hello", command=lambda: schListBox(E1.get(), schArray))
+"""Change the filter by changing the column name of the last argument of schListBox"""
+B = Button(top, text="Hello", command=lambda: schListBox(E1.get(),schArray,'dgp_code'))
 
 B.pack()
 top.mainloop()
