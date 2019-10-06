d6bfab7d4f4a8225b66c05203c16aeed6cf9fe36
puglord96 <thegamemaster123@gmail.com>
2019-10-06 15:17:35 +0800

    Update schInfoGUIFunction.py

--- schInfoGUIFunction.py
+++ schInfoGUIFunction.py
@@ -1,43 +1,37 @@
-import csv
+from Tkinter import *
+from schoolByCondition import *
 import schoolInfo
+
+
 # !/usr/bin/python
 
 
-import tkMessageBox
-from Tkinter import *
-
-def callTk():
-    top = Tk()
-
-    return top
-
-def messageBox(x):
-    top = callTk()
-    frame = Frame(top)
+def messageBox(x, y):
+    """messageBox generates the box which contains a string. x is the string to be printed, y is the tkinter
+    widget location. It is usually Tk(). DO NOT USE THIS FUNCTION TO CALL OUT THE BOX. FOR LOCAL USE ONLY"""
+    frame = Frame(y)
     frame.place(rely=0.1, relheight=0.6, relwidth=1.0)
     var = StringVar()
     label = Message(frame, textvariable=var, bd=6, relief=SUNKEN)
 
-    top.geometry("1000x500")
-    top.title("School Guide")
+    y.geometry("1000x500")
+    y.title("School Guide")
 
     var.set(x)
     label.pack(side=TOP)
     label.place(y=80, relwidth=1, height=200)
-    top.mainloop()
+    y.mainloop()
 
-def printInfo(x):
+
+def printInfo(x, y):
+    """printInfo generates the messageBox outside of the function. x is the search string and y is the tkinter
+    widget location. It is usually Tk(). Use this function to call out the messagebox"""
     try:
         if x == "":
-            messageBox("")
+            messageBox("",y)
         else:
             schstuff = schoolInfo.schoolstuff(x)
-            messageBox(schstuff)
+            messageBox(schstuff, y)
 
-    except:
-        messageBox("No such school found!")
-
-
-
-
-
+    except:  #DO NOT USE BARE EXCEPT
+        messageBox("No such school found!",y)
