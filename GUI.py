ccad1f90c9e4b484898a2c1989962bda2ea5d510
derekyjj <55313529+derekyjj@users.noreply.github.com>
2019-10-08 14:00:31 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,7 +1,6 @@
-
 from Tkinter import *
 import Tkinter as tk
-import schoolInfo
+from schoolInfo import *
 from schoolByCondition import *
 
 
@@ -12,7 +11,7 @@ class App(tk.Tk):
         container.grid()
 
         self.frames = {}
-        for F in (StartPage, SearchPage, SubjectPage, CutOffPage):
+        for F in (StartPage, SearchPage, SubjectPage, CutOffPage, LocationPage):
             pagename = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[pagename] = frame
@@ -44,11 +43,14 @@ class StartPage(tk.Frame):
                             command=lambda: controller.show_frame("SubjectPage"))
         button3 = tk.Button(self, text="Search By Cut Off Points", width=20,
                             command=lambda: controller.show_frame("CutOffPage"))
+        button4 = tk.Button(self, text="Search By Location", width=20,
+                            command=lambda: controller.show_frame("LocationPage"))
         quit = tk.Button(self, text="Quit", width=20, command=self.quit)
         button1.grid(row=1, column=2)
         button2.grid(row=2, column=2)
         button3.grid(row=3, column=2)
-        quit.grid(row=4, column=2)
+        button4.grid(row=4, column=2)
+        quit.grid(row=5, column=2)
         self.grid_columnconfigure((0, 4), weight=1)
 
 
@@ -57,16 +59,39 @@ class SearchPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+
+        def messageBox(x):
+            """messageBox generates the box which contains a string. x is the string to be printed, y is the tkinter
+            widget location. It is usually Tk(). DO NOT USE THIS FUNCTION TO CALL OUT THE BOX. FOR LOCAL USE ONLY"""
+            #label = Message(self, textvariable=var, bd=6, relief=SUNKEN)
+            label1 = Message(self, textvariable=var, bd=6, relief=SUNKEN)
+            label1.grid(column=1, row=4, columnspan=3, rowspan=2, sticky=(N, S, E, W))
+            #label.grid(column=1, row=4)
+            var.set(x)
+
+        def printInfo(x):
+            """printInfo generates the messageBox outside of the function. x is the search string and y is the tkinter
+            widget location. It is usually Tk(). Use this function to call out the messagebox"""
+            try:
+                if x == "":
+                    messageBox("")
+                else:
+                    schstuff = schoolstuff(x)
+                    messageBox(schstuff)
+
+            except:  # DO NOT USE BARE EXCEPT
+                messageBox("No such school found!")
         var = StringVar()
 
         L1 = tk.Label(self, text="Name of School")
         L1.grid(row=1, column=1)
         E1 = Entry(self, bd=5)
         E1.grid(row=2, column=1)
-        B1 = tk.Button(self, text="Search", command="printInfo(schstuff)")
+        B1 = tk.Button(self, text="Search", command=lambda: printInfo(E1.get()))
         B1.grid(row=3, column=1)
-        label1 = tk.Frame(self, borderwidth=5, relief="sunken", width=300, height=200)
-        label1.grid(column=1, row=4, columnspan=3, rowspan=2, sticky=(N, S, E, W))
+        messageBox("")
+        #label1 = tk.Frame(self, borderwidth=5, relief="sunken", width=300, height=200)
+        #label1.grid(column=1, row=4, columnspan=3, rowspan=2, sticky=(N, S, E, W))
         back = tk.Button(self, text="Back", width=20, command=lambda: controller.show_frame("StartPage"))
         quit = tk.Button(self, text="Quit", width=20, command=self.quit)
         back.grid(row=6, column=1)
@@ -101,6 +126,28 @@ class SubjectPage(tk.Frame):
         quit.grid(row=6, column=2)
 
         self.grid_columnconfigure((0, 7), weight=1)
+
+
+class LocationPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        var = StringVar()
+        label1 = tk.Frame(self, borderwidth=5, relief="sunken", width=300, height=200)
+        label1.grid(column=1, row=4, columnspan=3, sticky=(N, S, E, W))
+        L1 = tk.Label(self, text="Location: ")
+        L1.grid(row=0, column=2)
+        E1 = Entry(self, bd=5)
+        E1.grid(row=1, column=2)
+        B1 = tk.Button(self, text="Search", command="")
+        B1.grid(row=2, column=2)
+        label1 = tk.Frame(self, borderwidth=5, relief="sunken", width=300, height=200)
+        label1.grid(column=1, row=4, columnspan=3, sticky=(N, S, E, W))
+        back = tk.Button(self, text="Back", width=20, command=lambda: controller.show_frame("StartPage"))
+        quit = tk.Button(self, text="Quit", width=20, command=self.quit)
+        back.grid(row=5, column=2)
+        quit.grid(row=6, column=2)
 
 
 class CutOffPage(tk.Frame):
