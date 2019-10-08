96c08e909847b2c46c42c011768fcf9902d78a07
puglord96 <thegamemaster123@gmail.com>
2019-10-08 12:33:32 +0800

    search by condition using search box

--- schInfoGUIFunction.py
+++ schInfoGUIFunction.py
@@ -19,7 +19,7 @@ def messageBox(x, y):
 
     var.set(x)
     label.pack(side=TOP)
-    label.place(y=80, relwidth=1, height=200)
+    label.place(y=80, relwidth=1, height=300)
     y.mainloop()
 
 
