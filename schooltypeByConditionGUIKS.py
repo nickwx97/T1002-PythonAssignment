884543040c1c5daa71bc41c11b6c5f9c6af98799
puglord96 <thegamemaster123@gmail.com>
2019-10-08 14:13:32 +0800

    Update schooltypeByConditionGUIKS.py

--- schooltypeByConditionGUIKS.py
+++ schooltypeByConditionGUIKS.py
@@ -42,30 +42,51 @@ top = Tk()
 
 top.geometry("1000x500")
 
-L1 = Label(top, text="School Type")
+L1 = Label(top, text="School Area")
 L1.pack()
-E1 = Entry(top, bd=5)
-E1.pack()
 
 
 schArray = []
 
+typeCodeArray = []
+natureCodeArray = []
+sessionCodeArray = []
+schoolLevelArray = []
+areaarray = []
+
+
+"""You can change the column you want you populate in the dropdown box below here"""
+for x in schoolstuff("","type_code"):
+        if x[1][0]["type_code"] not in typeCodeArray:
+            typeCodeArray.append(x[1][0]["type_code"])
+
+
+variable = StringVar(top)
+variable.set("") # default value
+
+"""You can change the column you want you filter below here"""
+w = OptionMenu(top,variable,*typeCodeArray,command=lambda func: schListBox(variable.get(),schArray,'dgp_code'))
+w.pack()
+
 
 frame = Frame(top)
-frame.place(rely=0.5, relheight=0.6, relwidth=1.0)
+frame.place(y=230,height=300, relwidth=0.3,relx=0.5,anchor="center")
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side=RIGHT, fill=Y)
 
-Lb1 = Listbox(top, height=20, width=50)
+Lb1 = Listbox(frame, height=30, width=100)
 # appendArr('bedok',schArray,'dgp_code')
 """Lb1.bind contains the onclick event of the listBox. Lb1.curselection() is just the position number of each member 
 in the listbox i.e first member will be position number 0 and so on"""
 Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(schArray[Lb1.curselection()[0]][0],Toplevel()))  # Toplevel() just lets the function to be opened in a new window
-Lb1.pack()
 
-"""Creates an empty listbox when the program is opened"""
-# schListBox("","dgp_code")
+Lb1.pack(fill=X,expand=YES)
+
+Lb1.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=Lb1.yview)
 
 """Change the filter by changing the column name of the last argument of schListBox"""
-B = Button(top, text="Search", command=lambda: schListBox(E1.get(),schArray,'type_Code'))
+# B = Button(top, text="Search", command=lambda: schListBox(variable.get(),schArray,'dgp_code'))
 
-B.pack()
+# B.pack()
 top.mainloop()
