463979804e615d2f715c4dd744d4fcc43a928cf2
puglord96 <thegamemaster123@gmail.com>
2019-10-10 18:30:30 +0800

    Update schooltypeByConditionGUIKS.py

--- schooltypeByConditionGUIKS.py
+++ schooltypeByConditionGUIKS.py
@@ -4,6 +4,7 @@ from schoolByCondition import *
 """appendArr uses the function schoolstuff from schoolByCondition to populate the array for populating the listbox.
 If you want to change the filter condition, please change it in schoolByCondition.py"""
 
+overallList = schoolstuff("","type_code")
 
 def appendArr(x, y, z):
     del y[:]
@@ -15,26 +16,80 @@ def appendArr(x, y, z):
 
     return y
 
+def appendConditionArr(arr,type,level,gender,returnarr):
+
+    resultArr = []
+
+    if type:
+        for x in arr:
+            if level:
+                if gender:
+                    if type.lower() in x[1][0]["type_code"].lower() and level.lower() in x[1][0]["mainlevel_code"].lower() and gender.lower() in x[1][0]["nature_code"].lower():
+                        resultArr.append(x)
+
+                else:
+                    if type.lower() in x[1][0]["type_code"].lower() and level.lower() in x[1][0]["mainlevel_code"].lower():
+                        resultArr.append(x)
+
+            else:
+                if type in x[1][0]["type_code"]:
+                    resultArr.append(x)
 
 
-def schListBox(x,y,z):
+    elif level:
+        for x in arr:
+            if type:
+                if gender:
+                    if type.lower() in x[1][0]["type_code"].lower() and level.lower() in x[1][0]["mainlevel_code"].lower() and gender.lower() in x[1][0]["nature_code"].lower():
+                        resultArr.append(x)
+
+                else:
+                    if type.lower() in x[1][0]["type_code"].lower() and level.lower() in x[1][0]["mainlevel_code"].lower():
+                        resultArr.append(x)
+
+            else:
+                if level in x[1][0]["mainlevel_code"]:
+                    resultArr.append(x)
+
+
+    elif gender:
+        for x in arr:
+            if type:
+                if level:
+                    if type.lower() in x[1][0]["type_code"].lower() and level.lower() in x[1][0]["mainlevel_code"].lower() and gender.lower() in x[1][0]["nature_code"].lower():
+                        resultArr.append(x)
+
+                else:
+                    if gender.lower() in x[1][0]["nature_code"].lower() and type.lower() in x[1][0]["type_code"].lower():
+                        resultArr.append(x)
+
+            else:
+                if gender in x[1][0]["nature_code"]:
+                    resultArr.append(x)
+
+    returnarr.extend(resultArr)
+    return returnarr
+
+
+def schListBox(arr,x,y,z,returnarr):
     """schListBox populates the listbox based on the search substring. x is the search substring and y is the name of
     the array to be processed"""
-
-    if x=="":
-        del y[:]
+    funcArr = appendConditionArr(arr,x,y,z,returnarr)
+    if not x and not y and not z:
         Lb1.delete(0, END)
     else:
-        del y[:]
         Lb1.delete(0, END)
-        appendArr(x, y, z)
-        for schName in y:
+
+        for schName in funcArr:
             Lb1.insert(END, schName[0])
 
 
     # for z in y:
     #     print z
 
+
+def printtest(x):
+    print x
 
 top = Tk()
 
@@ -43,17 +98,13 @@ top = Tk()
 top.geometry("1000x500")
 
 
-
-
-schArray = []
-
 typeCodeArray = [""]
 natureCodeArray = [""]
 sessionCodeArray = [""]
 schoolLevelArray = [""]
 areaarray = [""]
 
-
+schArray = []
 """You can change the column you want you populate in the dropdown box below here"""
 for x in schoolstuff("","type_code"):
         if x[1][0]["type_code"] not in typeCodeArray:
@@ -82,6 +133,7 @@ variable2.set("") # default value
 
 """You can change the column you want you filter below here"""
 y = OptionMenu(schoollevelframe,variable2,*schoolLevelArray)
+y.bind('<<OptionMenuSelect>>', lambda event: appendConditionArr(overallList,variable.get(),variable2.get(),variable1.get(),schArray))
 y.pack()
 
 
@@ -95,6 +147,7 @@ variable.set("") # default value
 
 """You can change the column you want you filter below here"""
 w = OptionMenu(typecodeframe,variable,*typeCodeArray)
+w.bind('<<OptionMenuSelect>>', lambda event: appendConditionArr(overallList,variable.get(),variable2.get(),variable1.get(),schArray))
 w.pack()
 
 
@@ -109,9 +162,15 @@ variable1.set("") # default value
 
 """You can change the column you want you filter below here"""
 x = OptionMenu(naturecodeframe,variable1,*natureCodeArray)
+x.bind('<<OptionMenuSelect>>', lambda event: appendConditionArr(overallList,variable.get(),variable2.get(),variable1.get(),schArray))
 x.pack()
 
 
+
+"""Change the filter by changing the column name of the last argument of schListBox"""
+B = Button(top, text="Hello", command=lambda: schListBox(overallList,variable.get(),variable2.get(),variable1.get(),schArray))
+
+B.pack()
 
 frame = Frame(top)
 frame.place(y=230,height=300, relwidth=0.3,relx=0.5,anchor="center")
@@ -122,9 +181,10 @@ Lb1 = Listbox(frame, height=30, width=100)
 # appendArr('bedok',schArray,'dgp_code')
 """Lb1.bind contains the onclick event of the listBox. Lb1.curselection() is just the position number of each member 
 in the listbox i.e first member will be position number 0 and so on"""
-Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(schArray[Lb1.curselection()[0]][0],Toplevel()))  # Toplevel() just lets the function to be opened in a new window
+Lb1.bind('<<ListboxSelect>>', lambda event:printInfo(schArray[Lb1.curselection()[0]][0],Toplevel()))  # Toplevel() just lets the function to be opened in a new window
 
-Lb1.pack(fill=X,expand=YES)
+# printInfo(schArray[Lb1.curselection()[0]][0],Toplevel())
+Lb1.pack(fill=X,expand=1)
 
 Lb1.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=Lb1.yview)
