771598d5ba2fe1f9a28095c51e17f642b64f6c64
Nicholas Koh <nicholaskohwx@gmail.com>
2019-10-10 16:28:10 +0800

    Added unique subject list and multi filter by subjects

--- subjects-offered.py
+++ subjects-offered.py
@@ -1,9 +1,35 @@
-from csv import *
 from schoolByCondition import *
+
 
 class SubjectsOffered:
     def getDict(self):
         return self.__dict
+
+    def filterMultiSubs(self, filtered, result=None):
+        if result is None:
+            result = self.__dict
+        if len(filtered) == 0:
+            return result
+        else:
+            result2 = {}
+            for i in result:
+                for x in result[i]:
+                    if filtered[0].upper() == x[self.__headers[1]].upper() and i not in result2:
+                        result2.update({i: result.get(i)})
+        return self.filterMultiSubs(filtered[1:], result2)
+
+    def getUniqueSubjectList(self):
+        if self.__subList is not []:
+            return self.genUniqueSubjectList()
+        else:
+            return self.__subList
+
+    def genUniqueSubjectList(self):
+        for rows in self.__dict.values():
+            for cols in rows:
+                if not cols.get(self.__headers[1]) in self.__subList:
+                    self.__subList.append(cols.get(self.__headers[1]))
+        return self.__subList
 
     def getSubjectsBySchoolName(self, school_name):
         subjects = []
@@ -58,9 +84,9 @@ class SubjectsOffered:
         self.__dict = x[1]
         self.__headers = x[0]
 
-
     def __init__(self):
         # self.rw = openCSV(csvPath())
+        self.__subList = []
         self.__raw = openCSV("Data/subjects-offered.csv")
         self.__dict = None
         self.__headers = None
@@ -68,5 +94,7 @@ class SubjectsOffered:
 
 so = SubjectsOffered()
 so.createDict()
-print so.getSubjectsByLevel('secondary')
-
+# print so.getSubjectsByLevel('secondary')
+# print so.getUniqueSubjectList()
+for k, v in so.filterMultiSubs(['Physics', 'H1 Physics', 'Active Citizenry Education']).items():
+    print k, v
