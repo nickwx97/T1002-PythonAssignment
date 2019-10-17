b980dc4e1d097641a861bed5fb221207c501259a
elishazacarias <55310442+elishazacarias@users.noreply.github.com>
2019-10-17 16:43:41 +0800

    issue for no school in box fixed

--- ccaByConditionGUIbyDropdown.py
+++ ccaByConditionGUIbyDropdown.py
@@ -14,63 +14,70 @@ def appendArr(y, z):
         y.append(name)
 
     return y
+#
+#
+#
+# def schListBox(x,y,z):
+#     """schListBox populates the listbox based on the search substring. x is the search substring and y is the name of
+#     the array to be processed"""
+#     schlistarr = []
+#
+#     if x=="":
+#         del y[:]
+#         Lb1.delete(0, END)
+#     else:
+#         del y[:]
+#         Lb1.delete(0, END)
+#         appendArr(y, z)
+#
+#         for schName in y:
+#             for ccaName in cca.listCcaFromSch(schName[0]):
+#                 if x.upper() in ccaName.upper():
+#                     schlistarr.append(schName)
+#         del y[:]
+#         for names in schlistarr:
+#             y.append([names[0]])
+#             Lb1.insert(END, names[0])
+#
+#     # for z in y:
+#     #     print z
 
+def appendCCAs():
+    arr = cca.listCcaFromSch("")
+    ccaarray = []
+    for i in arr:
+        if i not in ccaarray:
+            ccaarray.append(i)
 
+    return sorted(ccaarray)
 
-def schListBox(x,y,z):
-    """schListBox populates the listbox based on the search substring. x is the search substring and y is the name of
-    the array to be processed"""
-    schlistarr = []
-
-    if x=="":
-        del y[:]
-        Lb1.delete(0, END)
+def listBox(selected, arr):
+    itemsarr = []
+    if selected == "":
+        del arr[:]
+        Lb1.delete(0,END)
     else:
-        del y[:]
+        del arr[:]
         Lb1.delete(0, END)
-        appendArr(y, z)
+        appendArr(selected, arr)
 
-        for schName in y:
-            for ccaName in cca.listCcaFromSch(schName[0]):
-                if x.upper() in ccaName.upper():
-                    schlistarr.append(schName)
-        del y[:]
-        for names in schlistarr:
-            y.append([names[0]])
-            Lb1.insert(END, names[0])
-
-
-
-
-
-
-    # for z in y:
-    #     print z
-
+    c = cca.searchDict(value=selected)
+    for items in sorted(c):
+        Lb1.insert(END, items[0])
 
 top = Tk()
-
-
-
 top.geometry("1000x500")
 
 L1 = Label(top, text="CCAs")
 L1.pack()
 
-_ccaarray = cca.listCcaFromSch("")
-ccaarray = []
-for i in _ccaarray:
-    if i not in ccaarray:
-        ccaarray.append(i)
 
-sorted_ccaarray = sorted(ccaarray)
-
-schArray = []
+ccaArray = []
 variable = StringVar(top)
 variable.set("") # default value
 
 """You can change the column you want you filter below here"""
-w = OptionMenu(top,variable,*sorted_ccaarray,command=lambda func: schListBox(variable.get(),schArray,'dgp_code'))
+w = OptionMenu(top,variable,*appendCCAs(),command=lambda func: listBox(variable.get(), ccaArray))
 w.pack()
 
 
@@ -80,10 +87,12 @@ scrollbar = Scrollbar(frame)
 scrollbar.pack(side=RIGHT, fill=Y)
 
 Lb1 = Listbox(frame, height=30, width=100)
+
 # appendArr('bedok',schArray,'dgp_code')
 """Lb1.bind contains the onclick event of the listBox. Lb1.curselection() is just the position number of each member 
 in the listbox i.e first member will be position number 0 and so on"""
-Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(schArray[Lb1.curselection()[0]][0],Toplevel()))  # Toplevel() just lets the function to be opened in a new window
+arr = appendArr(ccaArray, 'dgp_code')
+Lb1.bind('<<ListboxSelect>>', lambda event: printInfo(arr[Lb1.curselection()[0]][0],Toplevel()))  # Toplevel() just lets the function to be opened in a new window
 
 Lb1.pack(fill=X,expand=YES)
 
